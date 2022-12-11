1e2069e9a1b88ad60dfb27836d193bf6f1e05ad0
TheModdedChicken <logan.h.shaw@gmail.com>
2022-12-11 11:58:24 +0800

    Remove OneGrid

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
       />
 
       <main className={styles.main}>
-        <div className='z-30'>
+        <div className='relative z-30'>
           {Construction ? <UnderConstruction/>
           :(
             <div className={`flex flex-row items-center justify-center`}>
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
               </div>
               {/* ^^ ${styles.noMobile} ^^ */}
 
-              <div className={`elementRemove w-1 h-[30vh] rounded-full dark:bg-gray-800 bg-gray-300 mx-5`}></div>
+              <div className={`${styles.noMobile} ${styles.centerBar} w-1 h-[30vh] rounded-full dark:bg-gray-800 bg-gray-300 mx-5`}></div>
               {/* ^^ ${styles.noMobile} ${styles.centerBar} ^^ */}
 
               <div className='w-[35vw] max-w-[600px]'>
@@ -64,8 +64,7 @@ const Home: NextPage = () => {
                 <a className='text-[#3178C6] font-semibold'> TypeScript</a>, 
                 <a className='text-[#304CDC] font-semibold'> CSS</a>, 
                 <a className='text-[#D35836] font-semibold'> HTML</a>, and 
-                <a className='text-transparent bg-clip-text bg-gradient-to-r from-[#659AD2] via-[#00599C] to-[#004482] font-semibold'> C++</a>.<br/><br/>
-                I currently work as a Senior Engineer at <a className='font-semibold hover:underline text-[#7ED957]' href='https://onegr.id/'>OneGrid</a>.
+                <a className='text-transparent bg-clip-text bg-gradient-to-r from-[#659AD2] via-[#00599C] to-[#004482] font-semibold'> C++</a>.
                 </p>
                 <div className='flex flex-row mt-5'>
                   <a href='https://github.com/TheModdedChicken'><FontAwesomeIcon icon={faGithub} width={30} height={30} className='mr-5'/></a>
